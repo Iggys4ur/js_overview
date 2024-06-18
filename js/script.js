143c5d63cd12f6c085a3bf12f7be78278e3b1eb6
Iggys4ur <eswaind@gmail.com>
2024-06-18 15:07:35 -0400

    changed some whitespace and clarified some comments

--- js/script.js
+++ js/script.js
@@ -9,10 +9,8 @@ console.log(age < 30); // less than
 console.log(age == 30); // equals
 console.log(age == '28'); // double equals only checks value
 console.log(age === '28'); // triple equals checks both value and type
-
 console.log(age >= 25); // greater than or equal to
 console.log(age <= 25); // less than or equal to
-
 console.log(age != 27); // not equals checks only value
 console.log(age !== 27); // not equals checks both value and type
 
@@ -21,27 +19,31 @@ console.log(age !== 27); // not equals checks both value and type
 // Logical Operators return a value on either left or right of the operator
 console.log(age || 27 || names || null || -5 || undefined); // returns the first 'truthy' value or last term
 console.log(age && lastName && firstName && 0 && names); // returns the first 'falsey' value or last term
-var oldEnough = (age >= 21)
-// IF Conditionals trigger if the value passed is truthy
+var oldEnough = (age >= 21); // returns true if age is equal to or greater than 21
 
+// IF Conditionals trigger if the value passed is truthy
 if (firstName && lastName){
     alert("Received your full name!");
+    console.log("Received your full name!");
 }
-else if (!firstName){
+else if (!firstName){ // if firstName is not truthy
     alert("You must provide your first name!");
+    console.log("You must provide your first name!");
 }
-else if (!lastName){
+else if (!lastName){ // if lastName is not truthy
     alert("You must provide your last name!");
+    console.log("You must provide your last name!");
 }
-else {
+else { // if both firstName and LastName are not truthy
     alert("You must provide your full name!");
+    console.log("You must provide your full name!");
 }
 
-if(oldEnough) {
-    console.log("Welcome!")
-    alert('Welcome ' + firstName + '!')
+if(oldEnough) { // if oldEnough is truthy
+    console.log("Welcome!");
+    alert('Welcome ' + firstName + '!');
 }
-else {
-    console.log('Must be old enough to enter!')
-    alert('No chance...... ' + firstName)
+else { // if oldEnough is not truthy
+    console.log('Must be old enough to enter!');
+    alert('No chance...... ' + firstName);
 }
