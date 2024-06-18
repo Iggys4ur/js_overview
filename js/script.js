05032fec82e4d5b9940814d419c11b794bb7aaa4
Iggys4ur <eswaind@gmail.com>
2024-06-18 10:23:24 -0400

    added js/script.js and css/style.css

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+/* variables are assigned by calling
+        var <variable-name> = <assigned value>
+*/
+var userInitials = 'ESD'
+
+// Javascript works with / contains a set of different value types
+// strings are wrapped as 'String' value type
+// numbers are wrapped as 'Number' value type
+
+//numbers are written without quotes
+console.log(typeof 3)
+
+//strings are written inside quotes
+console.log(typeof '3')
+
+console.log(userInitials);
+
+//Javascript is capable of basic math (and more)
+console.log(5 + 5)
