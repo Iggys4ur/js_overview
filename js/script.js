a06d50a2e13d31d7a23bb181b1a38cfd0f0ff21d
Iggys4ur <eswaind@gmail.com>
2024-06-27 15:14:07 -0400

    updated script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
+
+// Target the h1 element by the ID
+const output = document.getElementById('key-output');
+
 function keyPressed(eventObj) {
+
     console.log(eventObj.key);
+
+    console.log(eventObj);
+
+    // Print the last key pressed
+    output.innerText = 'Pressed: ' + eventObj.key;
 
     if (eventObj.keyCode === 13) {
         console.log(eventObj.key);
         console.log('You have pressed the enter key')
     }
+
 }
 
 document.addEventListener('keydown', keyPressed)
