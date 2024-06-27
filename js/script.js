5d4ecc25e0c22df1fd7925ffb4c65bc114ee190d
Iggys4ur <eswaind@gmail.com>
2024-06-27 13:57:10 -0400

    added listener

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-// 
+function keyPressed(eventObj) {
+    console.log(eventObj.key);
+
+    if (eventObj.keyCode === 13) {
+        console.log(eventObj.key);
+        console.log('You have pressed the enter key')
+    }
+}
+
+document.addEventListener('keydown', keyPressed)
