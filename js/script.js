2c7e4399f3173bda982cbd980e3a2a6d2e728ca1
Iggys4ur <eswaind@gmail.com>
2024-07-02 04:46:56 -0400

    added whitespace

--- js/script.js
+++ js/script.js
@@ -13,39 +13,54 @@ function createNote(eventObj) {
     eventObj.preventDefault();
 
     const noteInput = document.querySelector('#note-input');
-
     const noteText = noteInput.value;
 
     const dateObj = new Date();
-
     const month = dateObj.getMonth();
-
     const date = dateObj.getDate();
-
     const year = dateObj.getFullYear();
-
     const dateStr = month + '/' + date + '/' + year;
-
     const altDateStr = `${month}/${date}/${year}`;
-
-    console.log(noteText);
-    console.log(dateStr);
-    console.log(altDateStr);
 
     // Note object stores the content and date of the note
     var noteObj = {
         note: noteText,
-        noteDate: altDateStr
+        noteDate: dateStr,
+        noteDateAlt: altDateStr
     }
 
     // Pull the old data from the database(localStorage) an empty array if no previous data has been stored
     const notes = JSON.parse(localStorage.getItem('notes')) || [];
 
+    console.log(notes[1]);
+
     // push the noteBlock to the notes array
     notes.push(noteObj);
 
     localStorage.setItem('notes', JSON.stringify(notes));
+
+    const container = document.getElementById('grid-container');
+    createGrid(5, 3);
 }
+
+
+function createGrid(rows, cols) {
+    const container = document.getElementById('grid-container');
+    const table = document.createElement('table');
+
+    for (let i = 0; i < rows; i++) {
+        const tr = document.createElement('tr');
+        for (let j = 0; j < cols; j++) {
+            const td = document.createElement('td');
+            td.textContent = `[ ${i},${j} ]`;
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+    container.appendChild(table);
+}
+
+createGrid(5, 3); // Create a 5x5 grid
 
 // set up necessary event listeners that should listen when the page loads
 
