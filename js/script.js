c6cf977f691159b42302a5c89d7cd0a4bcfb9690
Iggys4ur <eswaind@gmail.com>
2024-07-01 23:58:08 -0400

    updated script.js:getCharacters()

--- js/script.js
+++ js/script.js
@@ -1,32 +1,35 @@
 const charSelect = document.querySelector('#char-select');
+const searchBtn = document.querySelector('#search-btn');
+
 
 function getCharacters() {
-  const baseUrl = 'https://swapi.dev/api';
-  const url = baseUrl + '/people';
-  const val = this.value;
+    const baseUrl = 'https://swapi.dev/api';
+    const option = charSelect.value;
+    const url = `${baseUrl}/${option}`;
 
-  console.log(val);
+    console.log(option);
 
-  fetch(url)
-    .then(function (resObj) {
-      return resObj.json();
-    })
-    .then(function (data) {
-      const outputDiv = document.querySelector('.output');
+    fetch(url)
+        .then(function (resObj) {
+            // Parsing the JSON data from the response body
+            return resObj.json();
+        })
+        .then(function (data) {
+            const outputDiv = document.querySelector('.output');
 
-      data.results.forEach(function (char) {
-        outputDiv.insertAdjacentHTML('beforeend', `
+            data.results.forEach(function (char) {
+                outputDiv.insertAdjacentHTML('beforeend', `
           <div>
             <h3>${char.name}</h3>
             <p>Birth Year: ${char.birth_year}</p>
           </div>
         `);
-      })
-    })
+            })
+        })
 }
 
 function init() {
-  charSelect.addEventListener('change', getCharacters);
+    charSelect.addEventListener('click', getCharacters);
 }
 
 init();
